27362eddf278925627be838688a584e75f36a55b
Radya Al-Basha <47277078+RadyaAlbasha@users.noreply.github.com>
2019-06-03 15:35:42 +0200

    Task2
    hall view screen

---
+++ Task2HallView/Task2HallView/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Task2HallView
+//
+//  Created by Esraa Hassan on 6/3/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var buldingLabel: UILabel!
+    
+    @IBOutlet weak var floorLabel: UILabel!
+    
+    @IBOutlet weak var capacityLabel: UILabel!
+    
+    @IBOutlet weak var editBtn: UIButton!
+    
+    @IBOutlet weak var buldingView: UIView!
+    
+    @IBOutlet weak var floorView: UIView!
+    
+    @IBOutlet weak var capacityView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //rounded corners
+        roundedView(rView: editBtn)
+        roundedView(rView: buldingView)
+        roundedView(rView: floorView)
+        roundedView(rView: capacityView)
+        
+        
+    }
+    
+    func roundedView (rView : AnyObject){
+        rView.layer.cornerRadius = 10
+        rView.layer.masksToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
